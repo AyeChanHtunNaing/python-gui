2d9cf13739de423968900eb2ee44185c1096f681
Aye Chan Htun Naing <ayechanhtunnaing@icloud.com>
2025-03-22 15:38:53 +0700

    first tkinter app

---
+++ first-tkinter-app/app.py
@@ -0,0 +1,12 @@
+# In Python 3, the correct import is tkinter not Tkinter. Tkinter was the name of the module used for Python 2
+import tkinter as tk
+
+# ttk is the Python binding to the newer "themed widgets" added in Tk version 8.5
+from tkinter import ttk
+
+root = tk.Tk()
+label = ttk.Label(root, text="Hello, World!", padding=(30, 10))
+label.pack()
+
+root.update_idletasks()
+root.mainloop()
